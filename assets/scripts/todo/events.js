cd5e4f57e1bf4ce1e533c625231db57321f4c56f
yura1985 <yaremayurii@gmail.com>
2019-04-30 20:01:05 -0400

    Add font color to messages and some comments

--- assets/scripts/todo/events.js
+++ assets/scripts/todo/events.js
@@ -4,17 +4,21 @@ const getFormFields = require('./../../../lib/get-form-fields.js')
 const api = require('./api.js')
 const ui = require('./ui.js')
 
+// function to show list of all items for current user
 const onGetTodos = (event) => {
   event.preventDefault()
   api.getTodos()
     .then(ui.getTodosSuccess)
     .catch(ui.failure)
 }
+
+// function to hide the list of items
 const onClearTodos = (event) => {
   event.preventDefault()
   ui.clearTodos()
 }
 
+//  function to delete a one item from the list
 const onDeleteTodo = (event) => {
   event.preventDefault()
   const id = $(event.target).data('id')
@@ -24,6 +28,7 @@ const onDeleteTodo = (event) => {
     .catch(ui.failure)
 }
 
+// function to create one item
 const onCreateTodo = (event) => {
   event.preventDefault()
   const data = getFormFields(event.target)
@@ -33,6 +38,7 @@ const onCreateTodo = (event) => {
     .catch(ui.createTodoFailure)
 }
 
+// function to update a current item in the list
 const onUpdateTodo = (event) => {
   event.preventDefault()
   const data = getFormFields(event.target)
@@ -43,6 +49,7 @@ const onUpdateTodo = (event) => {
     .catch(ui.failure)
 }
 
+//  functionto mark item as completed
 const onCompleteTodo = (event) => {
   event.preventDefault()
   const data = getFormFields(event.target)
@@ -53,6 +60,7 @@ const onCompleteTodo = (event) => {
     .catch(ui.failure)
 }
 
+//  event handlers
 const addHandlers = () => {
   $('#getTodosButton').on('click', onGetTodos)
   $('#clearTodosButton').on('click', onClearTodos)
